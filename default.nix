b2c1c9293d546454c80de6f7b01bd0f3d27ea73d
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-25 17:59:42 +0300

    Switch to Meson

--- default.nix
+++ default.nix
@@ -2,11 +2,8 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
   name = "triggerd";
+  src = lib.cleanSource ./.;
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ meson ninja pkgconfig ];
   buildInputs = [ glib ];
-
-  buildCommand = ''
-    cc ${./triggerd.c} $(pkg-config --cflags --libs gio-2.0 glib-2.0) -o $out
-  '';
 }
