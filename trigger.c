305edeeb9c1fa1f5c71d66ad86ea40989a40db13
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-18 03:31:27 +0000

    Use RAII on sockets and addresses

--- trigger.c
+++ trigger.c
@@ -13,8 +13,9 @@
     fail(#expr, errno);
 
 int main() {
-  GSocket *sock = g_socket_new(G_SOCKET_FAMILY_IPV6, G_SOCKET_TYPE_DATAGRAM,
-                               G_SOCKET_PROTOCOL_UDP, NULL);
+  g_autoptr(GSocket) sock =
+      g_socket_new(G_SOCKET_FAMILY_IPV6, G_SOCKET_TYPE_DATAGRAM,
+                   G_SOCKET_PROTOCOL_UDP, NULL);
 
   if (sock == NULL)
     fail("g_socket_new", errno);
@@ -22,9 +23,10 @@ int main() {
   gint sock_min_rcvbuf;
 
   fail_if(g_socket_set_option(sock, SOL_SOCKET, SO_RCVBUF, 0, NULL) == FALSE);
-  fail_if(g_socket_get_option(sock, SOL_SOCKET, SO_RCVBUF, &sock_min_rcvbuf, NULL) == FALSE);
+  fail_if(g_socket_get_option(sock, SOL_SOCKET, SO_RCVBUF, &sock_min_rcvbuf,
+                              NULL) == FALSE);
 
-  GSocketAddress *addr = G_SOCKET_ADDRESS(g_inet_socket_address_new(
+  g_autoptr(GSocketAddress) addr = G_SOCKET_ADDRESS(g_inet_socket_address_new(
       g_inet_address_new_loopback(G_SOCKET_FAMILY_IPV6), DEFAULT_PORT));
 
   g_autofree gpointer buf = g_malloc0(sock_min_rcvbuf);
--- triggerd.c
+++ triggerd.c
@@ -20,8 +20,9 @@ int main(gint argc, char **argv) {
 
   argv++;
 
-  GSocket *sock = g_socket_new(G_SOCKET_FAMILY_IPV6, G_SOCKET_TYPE_DATAGRAM,
-                               G_SOCKET_PROTOCOL_UDP, NULL);
+  g_autoptr(GSocket) sock =
+      g_socket_new(G_SOCKET_FAMILY_IPV6, G_SOCKET_TYPE_DATAGRAM,
+                   G_SOCKET_PROTOCOL_UDP, NULL);
 
   if (sock == NULL)
     fail("g_socket_new", errno);
@@ -29,9 +30,10 @@ int main(gint argc, char **argv) {
   gint sock_min_rcvbuf;
 
   fail_if(g_socket_set_option(sock, SOL_SOCKET, SO_RCVBUF, 0, NULL) == FALSE);
-  fail_if(g_socket_get_option(sock, SOL_SOCKET, SO_RCVBUF, &sock_min_rcvbuf, NULL) == FALSE);
+  fail_if(g_socket_get_option(sock, SOL_SOCKET, SO_RCVBUF, &sock_min_rcvbuf,
+                              NULL) == FALSE);
 
-  GSocketAddress *addr = G_SOCKET_ADDRESS(g_inet_socket_address_new(
+  g_autoptr(GSocketAddress) addr = G_SOCKET_ADDRESS(g_inet_socket_address_new(
       g_inet_address_new_loopback(G_SOCKET_FAMILY_IPV6), DEFAULT_PORT));
 
   fail_if(g_socket_bind(sock, addr, TRUE, NULL) == FALSE);
